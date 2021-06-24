36376f9348cd26e246da81a8141be11e0954a508
Moumen Hallak <moumen95@gmail.com>
2021-06-24 00:38:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ BoardMember.prototype.veto = function (){
   return "No, I must disagree";
 };
 
-BoardMember.prototype.veto = function (){
-  return "No, I must disagree";
+BoardMember.prototype.approve = function (){
+  return "You can do that!";
 };
 
 BoardMember.prototype.approve = function (){
-  return "No, I must disagree";
+  return "You can do that!";
 };
