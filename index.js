fb2f6054d20c44d3e560e6c18022a4e7aa752b37
Moumen Hallak <moumen95@gmail.com>
2021-06-24 00:39:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ BoardMember.prototype.doCharity = function (){
 };
 
 BoardMember.prototype.releasePressStatement = function (){
-  return "I like to help people.";
+  return "You will see great things from Scuber.";
 };
